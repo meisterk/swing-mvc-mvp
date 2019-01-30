e65ab377d6cea983bb7143f295066a86b2711778
meisterk <franz@kohnlehome.de>
2019-01-30 09:06:28 +0100

    Add Controls to PlusMinusView

--- src/PlusMinusView.java
+++ src/PlusMinusView.java
@@ -7,9 +7,25 @@ public class PlusMinusView extends JFrame {
     private JLabel labelLautstaerke;
 
     public PlusMinusView() {
+        // komplettes Fenster
         setTitle("Lautstärke");
-        setSize(400, 100);
+        setSize(300, 100);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Gesamtlayout
+        JPanel panelGesamt = new JPanel(new BorderLayout());
+        buttonMinus = new JButton("-");
+        buttonPlus = new JButton("+");
+        labelLautstaerke = new JLabel("TEST");
+        labelLautstaerke.setHorizontalAlignment(SwingConstants.CENTER);
+
+        panelGesamt.add(buttonMinus, BorderLayout.WEST);
+        panelGesamt.add(labelLautstaerke, BorderLayout.CENTER);
+        panelGesamt.add(buttonPlus, BorderLayout.EAST);
+
+        getContentPane().add(panelGesamt);
         setVisible(true);
     }
 }
